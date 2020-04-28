41d56b2ef908f15e7c248719d895542ae9af1b82
E0227412 <30898676+E0227412@users.noreply.github.com>
2020-04-28 15:13:01 +0800

    Update MyStringStackTest.java

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -101,6 +101,7 @@ public class MyStringStackTest extends TestCase {
 	public void testStackNull() {
         String o1 = null;  
         MyStringStack stack = new MyStringStack();
+	stack.push (s1);	
         assertTrue (stack.isEmpty());
         stack.push (null);
         assertTrue (stack.isEmpty());
